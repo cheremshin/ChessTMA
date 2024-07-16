b1115c5859101a75a46a077b61ed54cc507d5167
cheremshin <sergey.cheremshin@gmail.com>
2024-07-16 19:37:19 +0700

    feat: Title block

---
+++ src/widgets/mainPage/Title.tsx
@@ -0,0 +1,17 @@
+import { widgetData } from "./Title.data";
+
+const Title = () => {
+    return (
+        <div className="
+            font-semibold
+            p-3
+            pl-[32px]
+            text-lg
+            whitespace-pre-line
+        ">
+            { widgetData.title }
+        </div>
+    );
+};
+
+export default Title;
