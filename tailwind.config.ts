8e9a0dd85f502d623af1151f36147ea447f3165b
cheremshin <sergey.cheremshin@gmail.com>
2024-07-16 19:34:03 +0700

    feat: Color presets for tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,11 +6,18 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+     colors: {
+       primary: "#000000",
+       background: "#FFFFFF",
+       accent: "#821F1F",
+       button: {
+        primary: "#606060",
+        hovered: "#000000",
+       },
+     },
+     backgroundImage: {
+      
+     },
     },
   },
   plugins: [],
