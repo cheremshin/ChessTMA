c95aafad24265beb850fe3e24d83a5bdfa855079
cheremshin <sergey.cheremshin@gmail.com>
2024-07-16 19:34:57 +0700

    feat: Choose task button

---
+++ src/widgets/mainPage/ChooseTheme.tsx
@@ -0,0 +1,18 @@
+const ChooseTheme = () => {
+    return (
+        <div className="
+            w-full
+            py-2
+            flex
+            justify-center
+            items-center
+            bg-black
+            rounded-l-[20px]
+            text-white
+        ">
+            Выбрать тему
+        </div>
+    )
+}
+
+export default ChooseTheme;
