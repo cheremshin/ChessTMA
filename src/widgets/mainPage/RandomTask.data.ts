2762eeafc8457884cab48949ac9eddc50ca8064a
cheremshin <sergey.cheremshin@gmail.com>
2024-07-26 16:01:20 +0700

    feat: Move random task data to local store

--- src/widgets/mainPage/RandomTask.data.ts
+++ src/widgets/mainPage/RandomTask.data.ts
@@ -2,5 +2,5 @@ export const widgetData = {
     title: "Решать случайную\nзадачу",
     description: "Вам дается задача на случайную тему в зависимости от вашего рейтинга",
     buttonText: "Решать",
-    link: "/#",
+    link: "/puzzle",
 }
