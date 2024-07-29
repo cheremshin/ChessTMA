f581e0c1fb5e587dd8592e3ee965467dfe46ee51
cheremshin <sergey.cheremshin@gmail.com>
2024-07-29 19:07:17 +0700

    feat: Page with board

--- src/pages/puzzlePage/page.tsx
+++ src/pages/puzzlePage/page.tsx
@@ -3,18 +3,10 @@ import { Board } from "@/widgets/puzzlePage/Board";
 import { BoardContextProvider } from "@/widgets/puzzlePage/BoardContext";
 
 
-const DEFAULT_POSITION = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-
-const PuzzlePage = async () => {
-    // Some fetching logic
-    const task = {
-        id: 1,
-        fen: "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2",
-    };
-
+const PuzzlePage = () => {
     return (
         <>
-            <BoardContextProvider task={task}>
+            <BoardContextProvider>
                 <Board />
             </BoardContextProvider>
             <Navbar />
