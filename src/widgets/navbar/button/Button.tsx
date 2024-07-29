78ec9fed1723e045fbe6dda8443c187547758cb5
cheremshin <sergey.cheremshin@gmail.com>
2024-07-29 19:04:41 +0700

    refactor: Improve code

--- src/widgets/navbar/button/Button.tsx
+++ src/widgets/navbar/button/Button.tsx
@@ -7,15 +7,18 @@ interface IMenuItem {
     item: IMenuLink;
 }
 
-const Button : FC<IMenuItem> = ({item}) => {
+const Button = ({item} : IMenuItem) => {
     return (
-       <div>
-           { item.bigButton ? <BigButton item={item} /> : <CommonButton item={item} /> }
-       </div>
+       <>
+            { item.bigButton
+                ? <BigButton item={item} />
+                : <CommonButton item={item} />
+            }
+       </>
     )
 };
 
-const BigButton : FC<IMenuItem> = ({item}) => {
+const BigButton = ({item}: IMenuItem) => {
     return (
         <Link href={item.url} className="relative h-[120px] -top-[17px] flex flex-col items-center justify-center">
             <Image className="border-[2px] border-[#C1C1C1] rounded-full p-[8px] bg-white" src={item.icon} alt="" width={70} height={70}/>
@@ -24,7 +27,7 @@ const BigButton : FC<IMenuItem> = ({item}) => {
     )
 }
 
-const CommonButton : FC<IMenuItem> = ({item}) => {
+const CommonButton= ({item}: IMenuItem) => {
     return (
         <Link href={item.url} className="flex flex-col items-center">
             <Image src={item.icon} alt="" width={35} height={35}/>
