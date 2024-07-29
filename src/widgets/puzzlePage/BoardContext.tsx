e6189b2ecd9ca1af3fed7344a2d593bf4e35e9dd
cheremshin <sergey.cheremshin@gmail.com>
2024-07-29 19:06:46 +0700

    feat: Add board context provider

--- src/widgets/puzzlePage/BoardContext.tsx
+++ src/widgets/puzzlePage/BoardContext.tsx
@@ -1,38 +1,61 @@
 import { 
     ReactNode,
     createContext,
+    useRef,
     useState
 } from "react";
 import { Chess } from "chess.js";
 import { BoardSide, Task } from "@/shared/types/types";
+import usePuzzlePage from "@/shared/hooks/usePuzzlePage";
 
 
 type Game = {
     fen: string;
+    solve: string[];
+    aiSteps: string[];
     game: Chess;
     side: BoardSide;
+    completed: boolean;
+    setCompleted: (completed: boolean) => void;
 };
 
 export const BoardContext = createContext<Game>({
     fen: "",
+    solve: [],
+    aiSteps: [],
     game: new Chess(),
-    side: "black",
+    side: "white",
+    completed: false,
+    setCompleted: () => {},
 });
 
 interface Props {
-    task: Task;
     children: ReactNode;
 };
 
-export const BoardContextProvider = ({ task, children }: Props) => {
-    const { fen } = task;
-    const [game, setGame] = useState(new Chess(fen));
+export const BoardContextProvider = ({ children }: Props) => {
+    const { task } = usePuzzlePage();
+    const { fen, solve } = task;
+    const game = new Chess(fen);
+
+    const [completed, setCompleted] = useState(false);
+
+    const splitMoves = solve.split(" ");
+
+    const playerSteps = splitMoves.filter((_, index) => index % 2 === 1);
+    const aiSteps = splitMoves.filter((_, index) => index % 2 === 0);
+
+    const side = useRef<BoardSide>(fen.split(" ")[1] === "w" ? "black" : "white");
 
     return (
         <BoardContext.Provider value={{
             fen,
+            solve: playerSteps,
+            aiSteps,
             game,
-            side: fen.split(" ")[1] === "w" ? "white" : "black",
+            side: side.current,
+            completed,
+            setCompleted,
         }}>
             {children}
         </BoardContext.Provider>
