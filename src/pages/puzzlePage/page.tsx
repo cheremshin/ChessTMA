76f3f7788af3558acf0cf9024a5ae2ce4d39d2e7
cheremshin <sergey.cheremshin@gmail.com>
2024-07-30 16:19:36 +0700

    feat: controls bar

--- src/pages/puzzlePage/page.tsx
+++ src/pages/puzzlePage/page.tsx
@@ -1,12 +1,16 @@
+import StatusBlock from "@/components/puzzlePage/StatusBlock";
 import Navbar from "@/widgets/navbar/Navbar";
 import { Board } from "@/widgets/puzzlePage/Board";
 import { BoardContextProvider } from "@/widgets/puzzlePage/BoardContext";
+import Header from "@/widgets/puzzlePage/Header";
 
 
 const PuzzlePage = () => {
     return (
         <>
             <BoardContextProvider>
+                <Header />
+                <StatusBlock />
                 <Board />
             </BoardContextProvider>
             <Navbar />
