6a27f43272831bca3cd1cf3334c3c4597071080d
cheremshin <sergey.cheremshin@gmail.com>
2024-07-30 16:19:02 +0700

    refactor: Change Game signature

--- src/shared/types/board.ts
+++ src/shared/types/board.ts
@@ -3,13 +3,29 @@ import { Chess } from "chess.js";
 
 export type BoardSide = "white" | "black";
 
+export enum SolveStatus {
+    inProgress,
+    success,
+    fail,
+};
+
+/**
+ * @param fen Строка расстановки фигур
+ * @param solve Последовательность ходов игрока для успешного прохождения
+ * @param aiSteps Ходы компьютера при правильной последовательности ходов
+ * @param side Сторона за которую играет игрок
+ * @param rating Рейтинг задачи
+ * @param status Статус прохождения пазла
+ * @param setStatus Мутатор статуса
+ */
 export type Game = {
     fen: string;
     solve: string[];
     aiSteps: string[];
-    game: Chess;
     side: BoardSide;
-    completed: boolean;
-    setCompleted: (completed: boolean) => void;
+    rating: number;
+    status: SolveStatus;
+    setStatus: (status: SolveStatus) => void;
+    completed: boolean
+    setCompleted: (status: boolean) => void;
 };
-
