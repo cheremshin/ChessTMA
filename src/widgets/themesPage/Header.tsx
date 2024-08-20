c9f6cb840410e57ea9ae698b171493274063e036
cheremshin <sergey.cheremshin@gmail.com>
2024-08-20 12:19:22 +0700

    fix: Trying to fix themes

--- src/widgets/themesPage/Header.tsx
+++ src/widgets/themesPage/Header.tsx
@@ -12,15 +12,18 @@ const Header = () => {
             bg-[#232323]
             text-white
             flex
-            justify-center
+            justify-between
             items-center
             font-semibold
             text-[22px]
+            px-10 // Adds horizontal padding to the entire header
         ">
-            <Link href="/" className="fixed left-10">
-                <Image src={ArrowLeft} alt="" width={30} height={30}/>
+            <Link href="/">
+                    <Image src={ArrowLeft} alt="Back" width={30} height={30}/>
             </Link>
-            <p>Список тем</p> 
+            <div className="flex-grow"></div> {/* This div takes up remaining space */}
+            <p className="text-center mx-auto pr-8">Список тем</p>
+            <div className="flex-grow"></div> {/* This div takes up remaining space */}
         </div>
     );
 };
